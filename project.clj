6cd85d4732cf52a50fc791e323fa5ca755865e49
Tassilo Horn <tsdh@gnu.org>
2015-05-12 12:29:41 +0200

    up

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "GNU General Public License, Version 3 (or later)"
             :url "http://www.gnu.org/licenses/gpl.html"
             :distribution :repo}
-  :dependencies [[funnyqt "0.47.7"]]
+  :dependencies [[funnyqt "0.48.0"]]
   :global-vars {*warn-on-reflection* true}
   :jvm-opts ^:replace ["-Xmx8G"])
